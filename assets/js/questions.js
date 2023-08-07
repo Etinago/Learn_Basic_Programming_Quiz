753047e19136b6149a66fd38ceb7b25c899979d3
Etinago <124711424+Etinago@users.noreply.github.com>
2023-08-07 22:05:45 +0100

    new repo structure

---
+++ assets/js/questions.js
@@ -0,0 +1,10 @@
+var javaQues = [{
+    que: "Inside which HTML element do we put the JavaScript____",
+    ans: [{text: "<js>", isCorrect: false },
+    {text: "<script>", isCorrect: true},
+    {text: "<javascript>", isCorrect: false},
+    {text: "<scripting>", isCorrect: false}
+]
+
+}];
+
