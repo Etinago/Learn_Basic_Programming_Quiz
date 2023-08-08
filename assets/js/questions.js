200756923a61c0457c3464883c1577bde4c8a216
Etinago <124711424+Etinago@users.noreply.github.com>
2023-08-08 08:48:58 +0100

    added question to js

--- assets/js/questions.js
+++ assets/js/questions.js
@@ -1,10 +1,23 @@
 var javaQues = [{
     que: "Inside which HTML element do we put the JavaScript____",
     ans: [{text: "<js>", isCorrect: false },
-    {text: "<script>", isCorrect: true},
-    {text: "<javascript>", isCorrect: false},
-    {text: "<scripting>", isCorrect: false}
-]
+    {text: "<script>", isCorrect: true },
+    {text: "<javascript>", isCorrect: false },
+    {text: "<scripting>", isCorrect: false }
+    ]
+},
+{
+    que: "Where is the correct place to insert a JavaScript?",
+    ans: [{text: "Both the <head> section and the <body> section are correct", isCorrect: false, isSelected: false },
+    {text: "The <head> section", isCorrect: false },
+    {text: "The <body> section", isCorrect: true}
+    ]   
+},
+{   
+    que: "How do you call a function named 'myFunction'?",
+    ans: [{text: "call function myFunction()", isCorrect: false, isSelected: false},
+    {text: "call myFunction()", isCorrect: false },
+    {text: "myFunction", isCorrect: true}
+    ]
 
-}];
-
+}]
